8f8656aa8ed40e984cd5bf20b6da8b2cf7d6a40f
Hudaibkhan <161836104+Hudaibkhan@users.noreply.github.com>
2024-03-06 23:03:53 +0500

    Add comments & Make Name List

---
+++ app.js
@@ -0,0 +1,20 @@
+"use strict";
+// QUESTION 10
+// My name is Hudaib
+// Date: 3/2/2024
+// This program will run simple code just like hello world
+console.log('hello world simple program');
+// This is Allam Iqbal words
+let quote = "Failure is not fatal until we surrender trying again is the key of glorious victory";
+console.log(`"Allama Iqbal, ${quote}" `);
+// QUESTION 11
+let nameList = ['Aliyan', 'Absar', 'Fabi'];
+for (let i = 0; i < nameList.length; i++) {
+    console.log(nameList[i]);
+}
+// QUESTION 12
+let cznList = ['Aliyan', 'Absar', 'Fabi'];
+let message = "What is your favourite Destination:";
+for (let i = 0; i < nameList.length; i++) {
+    console.log(message + cznList[i]);
+}
---
+++ app.ts
@@ -0,0 +1,21 @@
+// QUESTION 10
+// My name is Hudaib
+// Date: 3/2/2024
+// This program will run simple code just like hello world
+console.log('hello world simple program');
+// This is Allam Iqbal words
+ let quote: string ="Failure is not fatal until we surrender trying again is the key of glorious victory";
+ console.log(`"Allama Iqbal, ${quote}" `);
+
+// QUESTION 11
+let nameList:string[]=['Aliyan','Absar','Fabi'];
+for(let i=0;  i<nameList.length; i++){
+    console.log(nameList[i]);
+}
+
+// QUESTION 12
+let cznList:string[]=['Aliyan','Absar','Fabi'];
+let message:string="What is your favourite Destination:";
+for(let i=0; i<nameList.length; i++){
+    console.log(message+cznList[i])
+}
